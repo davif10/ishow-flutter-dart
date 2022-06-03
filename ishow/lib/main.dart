dc9d04a6dcf471946b2755b8dd49b072ba08047b
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2022-06-03 19:38:02 -0300

    Criando projeto app ishow
    - Criação do projeto
    - Configuração inicial e interface inicial

---
+++ ishow/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:ishow/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
